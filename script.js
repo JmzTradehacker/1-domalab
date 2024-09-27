a215306cf7f6dc5dabb22205e92a2e3a95062e1c
JmzTradehacker <japhet.mdn@gmail.com>
2024-09-26 21:12:35 -0700

    added menu links array of objects data structure

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 import "./styles.css";
+//Create Data Structure
+const menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
